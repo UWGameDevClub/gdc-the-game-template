1e23993a2d6f387a5beab2e11e84a941d3a48abe
everett-ripley <everett.ripley@gmail.com>
2026-09-11 18:34:28 -0400

    Separated leaderboard data management and ui logic

--- main/screens/Leaderboard/leaderboard.gd
+++ main/screens/Leaderboard/leaderboard.gd
@@ -1,15 +1,13 @@
 extends ScreenRoot
 
+signal request_online_leaderboards
+signal request_local_leaderboards
+
 @onready var local_leaderboard := $MarginContainer/VBoxContainer/HBoxContainer/LocalLeaderboardList
 @onready var today_leaderboard := $MarginContainer/VBoxContainer/HBoxContainer/TodayLeaderboardList
 @onready var alltime_leaderboard := $MarginContainer/VBoxContainer/HBoxContainer/AllTimeLeaderboardList
-@onready var http_request := $HTTPRequest
 @onready var name_input := $MarginContainer/VBoxContainer/MarginContainer2/HBoxContainer/CenterContainer/NameInput
 
-@export var local_data_manager : LocalDataManager
-
-const TALO_alltime_leaderboard_name : String = "All Time Leaderboard"
-const TALO_daily_leaderboard_name : String = "Daily Leaderboard"
 
 func update_local(scores:Array[ScoreResource]):
 	local_leaderboard.display_score_list(scores)
@@ -22,71 +20,27 @@ func update_alltime(scores:Array[ScoreResource]):
 
 func _on_visibility_changed():
 	if visible:
-		_build_local_entries()
-		http_request.test_connection()
-		await http_request.request_completed
-		if http_request.most_recent_result:
-			_get_online_leaderboards()
-		
+		_get_local_leaderboards()
+		_get_online_leaderboards()
 		
 
 func _get_online_leaderboards():
-	var page := 0
-	var done := false
-	while !done:
-		var options := Talo.leaderboards.GetEntriesOptions.new()
-		options.page = page
-		var alltime_res := await Talo.leaderboards.get_entries(TALO_alltime_leaderboard_name, options)
-		
-		if alltime_res == null:
-			print("ERROR LOADING ALLTIME LEADERBOARD")
-			break
-		
-		var is_last_page : bool = alltime_res.is_last_page
-		if is_last_page:
-			done = true
-		page += 1
-	
-	page = 0
-	done = false
-	while !done:
-		var options := Talo.leaderboards.GetEntriesOptions.new()
-		options.page = page
-		var daily_res := await Talo.leaderboards.get_entries(TALO_daily_leaderboard_name, options)
-		
-		if daily_res == null:
-			print("ERROR LOADING DAILY LEADERBOARD")
-			break
-		
-		var is_last_page : bool = daily_res.is_last_page
-		if is_last_page:
-			done = true
-		page += 1
-	
-	_build_online_entries()
+	request_online_leaderboards.emit()
 
-func _build_online_entries():
-	var daily_list : Array[ScoreResource]
-	var alltime_list : Array[ScoreResource]
-	if http_request.most_recent_result:
-		for entry in Talo.leaderboards.get_cached_entries(TALO_alltime_leaderboard_name):
-			alltime_list.append(_entry_to_score_res(entry))
-		for entry in Talo.leaderboards.get_cached_entries(TALO_daily_leaderboard_name):
-			daily_list.append(_entry_to_score_res(entry))
+func _get_local_leaderboards():
+	request_local_leaderboards.emit()
+
+func _build_online_entries(daily_list : Array[ScoreResource], alltime_list : Array[ScoreResource]):
 	update_alltime(alltime_list)
 	update_today(daily_list)
 	alltime_leaderboard.highlight_name(name_input.text)
 	today_leaderboard.highlight_name(name_input.text)
 
-func _build_local_entries():
-	if local_data_manager != null:
-		update_local(local_data_manager.get_scores())
-		local_leaderboard.highlight_name(name_input.text)
+func _build_local_entries(local_list : Array[ScoreResource]):
+	update_local(local_list)
+	local_leaderboard.highlight_name(name_input.text)
+	
 
-func _entry_to_score_res(entry:TaloLeaderboardEntry) -> ScoreResource:
-	var player_name : String = entry.player_alias.identifier
-	var player_score : int = entry.score
-	return ScoreResource.new(player_name, player_score)
 
 func highlight_score(name:String):
 	local_leaderboard.highlight_name(name)
@@ -108,8 +62,4 @@ func search_by_score_resource(scr_res : ScoreResource):
 
 func _on_update_timer_timeout():
 	if visible:
-		http_request.test_connection()
-		await http_request.request_completed
-		if http_request.most_recent_result:
-			_get_online_leaderboards()
-	
+		_get_online_leaderboards()
--- main/screens/Leaderboard/score_creator.gd
+++ main/screens/Leaderboard/score_creator.gd
@@ -1,8 +1,6 @@
 class_name ScoreCreator
 extends Control
 
-const TALO_alltime_leaderboard_name : String = "All Time Leaderboard"
-const TALO_daily_leaderboard_name : String = "Daily Leaderboard"
 
 # To be used to save scores locally
 signal score_created(score_resource:ScoreResource)
@@ -31,13 +29,6 @@ func enter_score():
 	name_input.hide()
 	var new_score := ScoreResource.new(name_input.text, score)
 	score_created.emit(new_score)
-	con_test.test_connection()
-	await con_test.request_completed
-	if con_test.most_recent_result:
-		await Talo.players.identify("username", name_input.text)
-		var res_alltime := await Talo.leaderboards.add_entry(TALO_alltime_leaderboard_name, new_score.score)
-		var res_daily := await Talo.leaderboards.add_entry(TALO_daily_leaderboard_name, new_score.score)
-		uploading_state_notification.hide()
 	hide()
 
 
