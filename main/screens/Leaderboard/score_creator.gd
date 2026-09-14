1240983948f5f95813bb0a143a5c7ab1de9f55ef
everett-ripley <everett.ripley@gmail.com>
2026-09-14 15:31:52 -0400

    Removed uploading hint

--- main/screens/Leaderboard/score_creator.gd
+++ main/screens/Leaderboard/score_creator.gd
@@ -8,7 +8,6 @@ signal score_created(score_resource:ScoreResource)
 @onready var enter_leaderboard_button := $VBoxContainer/CenterContainer/EnterLeaderboardButton
 @onready var name_input := $VBoxContainer/Control/CenterContainer/NameInput
 @onready var score_label := $VBoxContainer/ScoreDisplay/ScoreLabel
-@onready var uploading_state_notification := $UploadingStateNotification
 @onready var con_test := $ConnectionTest
 
 var score : int:
@@ -24,7 +23,6 @@ func open_creator(s:int):
 
 
 func enter_score():
-	uploading_state_notification.show()
 	enter_leaderboard_button.disabled = true
 	name_input.hide()
 	var new_score := ScoreResource.new(name_input.text, score)
